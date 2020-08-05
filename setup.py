965b819d9c15be928b56dfa52d21f4d7f82dc4ed
Adam Scibior <adscib@gmail.com>
2020-08-05 15:30:55 -0700

    Add minimal setup.py making srunner installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='srunner',
+    version='0.1.0',
+    url='https://github.com/carla-simulator/scenario_runner',
+    author='CARLA Team',
+    author_email='',
+    description='Not an official release',
+    packages=find_packages(),
+    install_requires=[],
+)
